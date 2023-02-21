82b0da358511557d266b81216863846daf736b59
Jonatha Fernandes <101746582+jonathafernandes@users.noreply.github.com>
2023-02-21 03:25:26 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+puts "Bem vindo ao Cookbook, sua rede social de receitas!"
+
+receits = []
+
+puts "Digite o nome da receita"
+nome = gets.chomp()
+
+receitas << nome
+
+puts "Receita de #{nome} cadastrada com sucesso!"
